14f6c2108edbe03c5af1439efa7227e48411eaca
farhannuari.romadhan <farhannuari.romadhan@ad.first-resources.com>
2023-03-03 22:50:28 +0700

    first push

---
+++ CMS Dashboard v1/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMS_Dashboard_v1.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Auth()
+        {
+            return RedirectToAction("Index","Home");
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index","Login");
+        }
+
+    }
+}
